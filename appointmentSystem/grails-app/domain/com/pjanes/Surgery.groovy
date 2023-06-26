df1e5b37eaba3ec3cec9ed686d518800f17fa84b
philapinyo <c0044807@my.shu.ac.uk>
2023-06-26 09:48:41 +0000

    Added constraints to Surgery

--- appointmentSystem/grails-app/domain/com/pjanes/Surgery.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Surgery.groovy
@@ -2,6 +2,25 @@ package com.pjanes
 
 class Surgery {
 
+    String name
+    String address 
+    String postcode
+    String telephone
+    int numberOfPatients
+    String description
+    String openingTime
+    Bool registeringNewPatients
+
     static constraints = {
+
+        name(size 1..100)
+        address(size 1..100)
+        postcode(size 1..10)
+        telephone(size 1..100)
+        numberOfPatients(100)
+        description(size 1..1000)
+        openingTime()
+        registeringNewPatients()
+
     }
 }
