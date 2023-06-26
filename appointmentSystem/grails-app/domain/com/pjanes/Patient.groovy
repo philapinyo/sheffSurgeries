53289d6876bc3238a9bc18ce87206ca93953c133
philapinyo <c0044807@my.shu.ac.uk>
2023-06-26 09:18:01 +0000

    Added constarints to patient class

--- appointmentSystem/grails-app/domain/com/pjanes/Patient.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Patient.groovy
@@ -2,6 +2,22 @@ package com.pjanes
 
 class Patient {
 
+    String patientName
+    String patientAddress
+    String patientResidence
+    Date patientDob
+    String patientID
+    Date dateRegistered
+    String patientPhone
+
     static constraints = {
+
+        patientName(size: 1..100)
+        patientAddress(size: 1..100)
+        patientResidence(size: 1..100)
+        patientDob()
+        patientID(unique: true, size 5..10)
+        dateRegistered()
+        patientPhone(size 7..15)
     }
 }
