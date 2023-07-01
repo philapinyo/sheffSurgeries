c8bf5e52aa9e379aed939f1cc40400b153dc6877
philapinyo <c0044807@my.shu.ac.uk>
2023-07-01 21:48:02 +0000

    Added constraints for Appointment

--- appointmentSystem/grails-app/domain/com/pjanes/Appointment.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Appointment.groovy
@@ -2,6 +2,14 @@ package com.pjanes
 
 class Appointment {
 
+    Date appDateTime
+    int appDuration
+    String roomNumber
+
     static constraints = {
+
+        appDateTime()
+        appDuration(size 1..100)
+        roomNumber(size 1..100)
     }
 }
