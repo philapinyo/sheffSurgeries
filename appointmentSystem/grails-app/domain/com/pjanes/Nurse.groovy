1c36580a5ec85f8cb2be64f4fba55f5b2454a38b
philapinyo <c0044807@my.shu.ac.uk>
2023-07-01 21:41:06 +0000

    Added constraints for Nurse

--- appointmentSystem/grails-app/domain/com/pjanes/Nurse.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Nurse.groovy
@@ -2,6 +2,19 @@ package com.pjanes
 
 class Nurse {
 
+    String nurseName
+    String qualifications
+    String nurseEmail
+    String nurseOffice
+    String nursePhone
+
     static constraints = {
+
+        nurseName(size 1..100)
+        qualifications(size 1..100)
+        nurseEmail(size 1..100)
+        nurseOffice(size 1..100)
+        nursePhone(size 1..100)
+        
     }
 }
