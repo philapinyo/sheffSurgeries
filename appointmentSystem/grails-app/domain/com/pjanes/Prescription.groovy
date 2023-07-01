5ae455455df72bf5a7b8a8e61f77de179d5a809e
philapinyo <c0044807@my.shu.ac.uk>
2023-07-01 21:37:22 +0000

    Added constraints for Prescription

--- appointmentSystem/grails-app/domain/com/pjanes/Prescription.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Prescription.groovy
@@ -2,6 +2,20 @@ package com.pjanes
 
 class Prescription {
 
+    String prescriptionNumber
+    String medicine
+    int daysSupply
+    String totalCost
+    Date dateIssued
+    Bool patientPaying
+
     static constraints = {
+
+        prescriptionNumber(size 1..100)
+        medicine(size 1..100)
+        daysSupply(size 0..100)
+        dateIssued()
+        patientPaying()
+        
     }
 }
