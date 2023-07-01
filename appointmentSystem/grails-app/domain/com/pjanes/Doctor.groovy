5a58f1a0e0e8bcebc4d6caa7df342e11d7a0d2ec
philapinyo <c0044807@my.shu.ac.uk>
2023-07-01 21:45:20 +0000

    Added constraints for doctor

--- appointmentSystem/grails-app/domain/com/pjanes/Doctor.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Doctor.groovy
@@ -2,6 +2,24 @@ package com.pjanes
 
 class Doctor {
 
+    String doctorName
+    String qualification
+    String position
+    String doctorEmail
+    String password
+    String doctorOffice
+    String doctorPhone
+    String bio
+
     static constraints = {
+
+        doctorName(size 1..100)
+        qualification(size 1..100)
+        position(size 1..100)
+        doctorEmail(size 1..100)
+        password(size 1..100)
+        doctorOffice(size 1..100)
+        doctorPhone(size 1..100)
+        bio(size 1..1000)
     }
 }
