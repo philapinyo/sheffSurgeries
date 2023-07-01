fa6011baeb490b177f12488621c807dec9d30a37
philapinyo <c0044807@my.shu.ac.uk>
2023-07-01 21:29:55 +0000

    Added Receptionist constraints

--- appointmentSystem/grails-app/domain/com/pjanes/Receptionist.groovy
+++ appointmentSystem/grails-app/domain/com/pjanes/Receptionist.groovy
@@ -2,6 +2,19 @@ package com.pjanes
 
 class Receptionist {
 
+    String recepName
+    String recepEmail
+    String recepUsername
+    String recepPasssword
+    String recepPhone
+
     static constraints = {
+
+        recepName(size 1..100)
+        recepEmail(size 1..100)
+        recepUsername(size 1..100)
+        recepPasssword(size 1..100)
+        recepPhone(size 1..100)
+
     }
 }
